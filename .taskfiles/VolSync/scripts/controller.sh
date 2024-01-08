f2c85f9e11f0cd2c34d8b5bdc99bede138202694
Steven Kreitzer <skre@skre.me>
2024-01-08 15:26:45 -0600

    chore(github): house keeping

---
+++ .taskfiles/VolSync/scripts/controller.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+APP=$1
+NAMESPACE="${2:-default}"
+
+is_deployment() {
+    kubectl -n "${NAMESPACE}" get deployment "${APP}" >/dev/null 2>&1
+}
+
+is_statefulset() {
+    kubectl -n "${NAMESPACE}" get statefulset "${APP}" >/dev/null 2>&1
+}
+
+if is_deployment; then
+    echo "deployment.apps/${APP}"
+elif is_statefulset; then
+    echo "statefulset.apps/${APP}"
+else
+    echo ""
+fi
