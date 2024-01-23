6209cd436406447565f29b288e157a24673fbfd0
Steven Kreitzer <skre@skre.me>
2024-01-23 15:08:17 -0600

    chore(qb): update xseed config
    Signed-off-by: Steven Kreitzer <skre@skre.me>

--- kubernetes/apps/media/qbittorrent/app/configs/config.js
+++ kubernetes/apps/media/qbittorrent/app/configs/config.js
@@ -10,13 +10,17 @@ module.exports = {
   qbittorrentUrl: "http://localhost:8080",
 
   torznab: [
-    `http://prowlarr.media.svc.cluster.local:9696/4/api?apikey=$${process.env.PROWLARR_API_KEY}`, // blu
-    `http://prowlarr.media.svc.cluster.local:9696/2/api?apikey=$${process.env.PROWLARR_API_KEY}`, // fl
-    `http://prowlarr.media.svc.cluster.local:9696/6/api?apikey=$${process.env.PROWLARR_API_KEY}`, // hdt
-    `http://prowlarr.media.svc.cluster.local:9696/5/api?apikey=$${process.env.PROWLARR_API_KEY}`, // st
-    `http://prowlarr.media.svc.cluster.local:9696/1/api?apikey=$${process.env.PROWLARR_API_KEY}`, // td
-    `http://prowlarr.media.svc.cluster.local:9696/3/api?apikey=$${process.env.PROWLARR_API_KEY}`, // tl
-    `http://prowlarr.media.svc.cluster.local:9696/7/api?apikey=$${process.env.PROWLARR_API_KEY}`, // ts
+    `http://prowlarr.media.svc.cluster.local:9696/10/api?apikey=$${process.env.PROWLARR_API_KEY}`, // ant
+    `http://prowlarr.media.svc.cluster.local:9696/9/api?apikey=$${process.env.PROWLARR_API_KEY}`,  // at
+    `http://prowlarr.media.svc.cluster.local:9696/4/api?apikey=$${process.env.PROWLARR_API_KEY}`,  // blu
+    `http://prowlarr.media.svc.cluster.local:9696/2/api?apikey=$${process.env.PROWLARR_API_KEY}`,  // fl
+    `http://prowlarr.media.svc.cluster.local:9696/6/api?apikey=$${process.env.PROWLARR_API_KEY}`,  // hdt
+    `http://prowlarr.media.svc.cluster.local:9696/11/api?apikey=$${process.env.PROWLARR_API_KEY}`, // ipt
+    `http://prowlarr.media.svc.cluster.local:9696/8/api?apikey=$${process.env.PROWLARR_API_KEY}`,  // phd
+    `http://prowlarr.media.svc.cluster.local:9696/5/api?apikey=$${process.env.PROWLARR_API_KEY}`,  // st
+    `http://prowlarr.media.svc.cluster.local:9696/1/api?apikey=$${process.env.PROWLARR_API_KEY}`,  // td
+    `http://prowlarr.media.svc.cluster.local:9696/3/api?apikey=$${process.env.PROWLARR_API_KEY}`,  // tl
+    `http://prowlarr.media.svc.cluster.local:9696/7/api?apikey=$${process.env.PROWLARR_API_KEY}`,  // ts
   ],
 
   apiAuth: false,
